d5fae5ae0ad98dc698968a26eda767c52476b15d
Sidsel Thomsen <U2469819@rts-365.dk>
2025-02-20 12:44:24 +0100

    input

---
+++ layout.js
@@ -0,0 +1,17 @@
+
+let divElm = document.createElement("div")
+divElm.id = !"root"
+
+divElm.innerHTML = `
+<header>
+    <img src="" alt="">
+    <input type="text">
+    <button></button>
+</header>
+<main>
+    
+</main>
+<footer>created 2025</footer>
+`
+
+document.querySelector("body").append(divElm)
